29e8a384dec96a7bdc4ebe6cca2bded9d95607e5
YingxiZhang0727 <yingxi202407@163.com>
2026-05-19 12:24:18 +0200

    first website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<h1>Italy Travel Guide</h1>
+<p>Discover three beautiful cities in Italy.</p>
+
+<nav>
+  <a href="/perugia">Perugia</a>
+  <a href="/milano">Milano</a>
+  <a href="/roma">Roma</a>
+</nav>
+
+<style>
+  h1 {
+    font-size: 48px;
+    color: darkred;
+  }
+
+  nav {
+    display: flex;
+    gap: 20px;
+    margin-top: 30px;
+  }
+
+  a {
+    font-size: 24px;
+    color: black;
+  }
+</style>
